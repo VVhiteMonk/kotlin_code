286ec73ba040e11d7cc28b1c902976f6c087997f
Vitalijus Rusakevic <vvhitemonk@gmail.com>
2024-01-22 22:04:18 +0200

    lesson II

---
+++ src/main/kotlin/lesson_2.kt
@@ -0,0 +1,18 @@
+fun main(){
+    println("Lesson 2")
+
+    val a = 5
+    val b = 7
+
+    val sum = a + b
+
+    //println(a + b)
+    println(sum)
+    println(sum::class.simpleName) // Int
+
+    val  boom = "boom"
+    println( boom + " is type -" + boom::class.simpleName)
+
+    println(10 % 3)
+    println(10 % 2)
+}
