0fea79bc0b24d6941a6a6d9f15677740961601a0
Vitalijus Rusakevic <vvhitemonk@gmail.com>
2024-01-22 19:23:05 +0200

    variables val and var

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("I love kotlin language. ");
+fun main() {
+    //println("Hello World!")
+
+    val numberOfOrders = 42
+    println(numberOfOrders)
+
+    var notificationText = "don't forget me!"
+    println(notificationText)
+
+    notificationText = "remember me"
+    println(notificationText)
 }
